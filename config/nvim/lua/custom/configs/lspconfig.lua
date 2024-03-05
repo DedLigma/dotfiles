e613e49f5b15125c014b625a29c438e33e120267
dedligma <dedligma@192.168.1.3>
2024-03-05 13:29:32 +0300

    upd nvim

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,17 +4,25 @@ local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
 
-lspconfig.clangd.setup {
-  on_attach = function(client, bufnr)
-    client.server_capabilities.signatureHelpProvider = false
-    on_attach(client, bufnr)
-  end,
-  capabilities = capabilities,
-}
-
+lspconfig.clangd.setup({
+	on_attach = function(client, bufnr)
+		client.server_capabilities.signatureHelpProvider = false
+		on_attach(client, bufnr)
+	end,
+	capabilities = capabilities,
+})
 
 lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"python"},
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "python" },
 })
+
+local servers = { "lua_ls", "neocmake" }
+
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
+end
--- import.sh
+++ import.sh
@@ -17,6 +17,15 @@ NC='\033[0;m'
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 
+if [ "$(ls -A ./config/)" ]; then
+  echo "Config folder is not empty."
+  read -p "Delete them? [y/n]: " answer
+  case "$answer" in 
+    [Yy]* ) rm -rf ./config/*;;
+    * ) echo "";;
+  esac
+fi
+
 echo -e "${GREEN}Start${NC} import ${BLUE}.zshrc ${NC} files\n"
 import_function ~/.zshrc ./
 echo
@@ -25,12 +34,6 @@ import_function ~/.config/kitty/kitty.conf ./config/kitty/
 import_function ~/.config/kitty/open-actions.conf ./config/kitty/
 import_function ~/.config/kitty/current-theme.conf ./config/kitty/
 import_function ~/.config/kitty/Material Dark.conf ./config/kitty/
-echo
-echo -e "${GREEN}Start${NC} import ${BLUE}LunarVim${NC} config files\n"
-import_function ~/.config/lvim/config.lua ./config/lvim/
-import_function ~/.config/lvim/lua/my_plugins.lua ./config/lvim/lua/
-import_function ~/.config/lvim/lua/config_c_cpp.lua ./config/lvim/lua/
-import_function ~/.config/lvim/lua/config_python.lua ./config/lvim/lua/
 echo
 echo -e "${GREEN}Start${NC} import ${BLUE}Yazi${NC} config files\n"
 import_function ~/.config/yazi/yazi.toml ./config/yazi/
@@ -41,21 +44,21 @@ import_function ~/.config/nvim/init.lua ./config/nvim/
 import_function ~/.config/nvim/lazy-lock.json ./config/nvim/
 import_function ~/.config/nvim/init.lua.bak ./config/nvim/
 
-import_function ~/.config/nvim/lua/core/init.lua ./config/nvim/core/
-import_function ~/.config/nvim/lua/core/utils.lua ./config/nvim/core/
-import_function ~/.config/nvim/lua/core/mappings.lua ./config/nvim/core/
-import_function ~/.config/nvim/lua/core/bootstrap.lua ./config/nvim/core/
-import_function ~/.config/nvim/lua/core/default_config.lua ./config/nvim/core/
+import_function ~/.config/nvim/lua/core/init.lua ./config/nvim/lua/core/
+import_function ~/.config/nvim/lua/core/utils.lua ./config/nvim/lua/core/
+import_function ~/.config/nvim/lua/core/mappings.lua ./config/nvim/lua/core/
+import_function ~/.config/nvim/lua/core/bootstrap.lua ./config/nvim/lua/core/
+import_function ~/.config/nvim/lua/core/default_config.lua ./config/nvim/lua/core/
 
-import_function ~/.config/nvim/lua/plugins/init.lua ./config/nvim/plugins/
-import_function ~/.config/nvim/lua/plugins/configs/cmp.lua ./config/nvim/plugins/configs/
-import_function ~/.config/nvim/lua/plugins/configs/mason.lua ./config/nvim/plugins/configs/
-import_function ~/.config/nvim/lua/plugins/configs/others.lua ./config/nvim/plugins/configs/
-import_function ~/.config/nvim/lua/plugins/configs/nvimtree.lua ./config/nvim/plugins/configs/
-import_function ~/.config/nvim/lua/plugins/configs/lazy_nvim.lua ./config/nvim/plugins/configs/
-import_function ~/.config/nvim/lua/plugins/configs/lspconfig.lua ./config/nvim/plugins/configs/
-import_function ~/.config/nvim/lua/plugins/configs/telescope.lua ./config/nvim/plugins/configs/
-import_function ~/.config/nvim/lua/plugins/configs/treesitter.lua ./config/nvim/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/init.lua ./config/nvim/lua/plugins/
+import_function ~/.config/nvim/lua/plugins/configs/cmp.lua ./config/nvim/lua/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/configs/mason.lua ./config/nvim/lua/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/configs/others.lua ./config/nvim/lua/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/configs/nvimtree.lua ./config/nvim/lua/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/configs/lazy_nvim.lua ./config/nvim/lua/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/configs/lspconfig.lua ./config/nvim/lua/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/configs/telescope.lua ./config/nvim/lua/plugins/configs/
+import_function ~/.config/nvim/lua/plugins/configs/treesitter.lua ./config/nvim/lua/plugins/configs/
 
 import_function ~/.config/nvim/lua/custom/mappings.lua ./config/nvim/lua/custom/
 import_function ~/.config/nvim/lua/custom/chadrc.lua ./config/nvim/lua/custom/
